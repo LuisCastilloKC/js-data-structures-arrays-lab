e733090225d781ec3e56db443ce6e14a9ceaba41
Luis Castillo <mr.luiscastillo@gmail.com>
2021-06-16 23:20:31 -0400

    commit tree eae8571fc6b0198b2ca09793ea0a0b75c0872238

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    
+    drivers.push(name);
+
+    return drivers
+}
+
+function destructivelyPrependDriver(name) {
+    
+    drivers.unshift(name);
+
+    return drivers
+}
+
+function destructivelyRemoveLastDriver(name) {
+    
+    drivers.pop(name);
+
+    return drivers
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    
+    drivers.shift(name);
+
+    return drivers
+}
+
+function appendDriver(name) {
+
+    return [...drivers, name]
+}
+
+function prependDriver(name) {
+
+    return [name, ...drivers]
+}
+
+function removeLastDriver(name) {
+
+    return drivers.slice(0, -1)
+}
+
+function removeFirstDriver(name) {
+
+    return drivers.slice(1)
+}
